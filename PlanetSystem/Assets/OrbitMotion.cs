9894dfc5cfcd5f5e818dc78e9e2ef70ae3e8f8a0
ravisharma <ravisharma911993@gmail.com>
2017-11-03 22:36:44 +0530

    Inital commit for planetory system

---
+++ PlanetSystem/Assets/OrbitMotion.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbitMotion : MonoBehaviour {
+
+    public Transform orbitingObject;
+    public Ellipse orbitPath;
+
+    [Range(0f, 1f)]
+    public float orbitProgress = 0f;
+    public float orbitPeriod = 3f;
+    public bool isOrbitActive = true;
+
+	// Use this for initialization
+	void Start () {
+        if (null == orbitingObject) {
+            isOrbitActive = false;
+            return;
+        }
+        SetOrbitingObjectPosition();
+        StartCoroutine(AnimateOrbit());
+    }
+
+    void SetOrbitingObjectPosition() {
+        Vector2 orbitPos = orbitPath.Evaluate(orbitProgress);
+        orbitingObject.localPosition = new Vector3(orbitPos.x, 0, orbitPos.y);
+    }
+
+    IEnumerator AnimateOrbit() {
+        if (orbitPeriod < 0.1f) {
+            orbitPeriod = 0.1f;
+        }
+        float orbitSpeed = 1f / orbitPeriod;
+        while (isOrbitActive) {
+            orbitProgress += Time.deltaTime * orbitSpeed;
+            orbitProgress %= 1f;
+            SetOrbitingObjectPosition();
+            yield return null;
+        }
+    }
+}
